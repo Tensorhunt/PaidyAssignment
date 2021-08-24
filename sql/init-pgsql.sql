e0b735b3093aa9368a44d15e7bd76d1c38bb922b
Niraj Desai <nirajdesai@Nirajs-MacBook-Pro.local>
2021-08-24 10:32:28 +0900

    Initial Commit

---
+++ sql/init-pgsql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE accounts (
+    "SeriousDlqin2yrs" BOOLEAN,
+    "RevolvingUtilizationOfUnsecuredLines" FLOAT,
+    "age" INT,
+    "NumberOfTime30-59DaysPastDueNotWorse" INT,
+    "DebtRatio" FLOAT,
+    "MonthlyIncome" INT,
+    "NumberOfOpenCreditLinesAndLoans" INT,
+    "NumberOfTimes90DaysLate" INT,
+    "NumberRealEstateLoansOrLines" INT,
+    "NumberOfTime60-89DaysPastDueNotWorse" INT,
+    "NumberOfDependents" INT
+);
